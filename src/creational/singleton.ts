657cd3cce11a697ab2224280c1d08619bb5b13bc
Vlas1d <vlasid753@gmail.com>
2023-05-11 01:22:02 +0300

    singleton (code)

---
+++ src/creational/singleton.ts
@@ -0,0 +1,35 @@
+enum Language {
+    UA = "Ukrainian",
+    EN = "English",
+    FR = "French"
+}
+
+class Settings {
+    private static instance: Settings; //Чому статичні властивості не можна вказати в інтерфейсі
+
+    sound: boolean;
+    music: boolean;
+    lang: Language;
+    wifi: boolean;
+
+    private constructor() {
+        this.sound = true;
+        this.music = false;
+        this.lang = Language.UA;
+        this.wifi = true;
+    }
+
+    static getInstance(): Settings {
+        return this.instance ??= new Settings();
+    }
+
+    setTheme(theme: string): void {
+        console.log(`Change theme to ${theme}`);
+    }
+}
+
+const settings1 = Settings.getInstance();
+const settings2 = Settings.getInstance();
+
+console.log(settings1);
+console.log(settings1.setTheme('Light'));
